d7c0437ef1d04f2cbf4758ed7e9b97690188eed4
oper29a@gmail.com <oper29a@gmail.com>
2015-11-13 10:00:32 +0500

    Base migration.

---
+++ migrations/m151113_042051_books.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151113_042051_books extends Migration
+{
+    public function up()
+    {
+        $this->createTable('books',[
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_TEXT.' NOT NULL DEFAULT ""',
+            'date_create' => Schema::TYPE_DATE.' NOT NULL',
+            'date_update' => Schema::TYPE_DATE.' NOT NULL',
+            'preview' => Schema::TYPE_TEXT.' NOT NULL',
+            'date' => Schema::TYPE_DATE.' NOT NULL',
+            'author_id' => Schema::TYPE_INTEGER.' NOT NULL',
+        ]);
+        $this->createTable('authors', [
+            'id' => Schema::TYPE_PK,
+            'firstname' => Schema::TYPE_TEXT.' NOT NULL DEFAULT ""',
+            'lastname' => Schema::TYPE_TEXT.' NOT NULL DEFAULT ""',
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('books');
+        $this->dropTable('authors');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
